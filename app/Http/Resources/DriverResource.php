ee6885a0f0006ced61140ea052a51a5bd942ba56
Roarkiner <l.dujour@it-students.fr>
2021-09-16 16:09:18 +0200

    Driver Resource done

--- app/Http/Resources/DriverResource.php
+++ app/Http/Resources/DriverResource.php
@@ -14,9 +14,6 @@ class DriverResource extends JsonResource
      */
     public function toArray($request)
     {
-        // return parent::toArray($request);
-        return [
-            'id' => $this->driverId,
-        ];
+        return parent::toArray($request);
     }
 }
